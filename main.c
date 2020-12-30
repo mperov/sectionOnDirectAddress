71915395f099e74fb8deca0b28e479ff19255700
Maksim Perov <coder@frtk.ru>
2020-12-30 19:28:58 +0300

    building are simplified

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #define SIZE_BUF 20
 
-char buff[SIZE_BUF]__attribute__((section("my_section")));
+char buff[SIZE_BUF]__attribute__((section(".my_section")));
 
 int main() {
     strcpy(buff, "Hello, world!\n");
